f8188c4a10b8020ce58d9084ad5224d5de1d9433
Damon Lemmons <damonlemmons@Damons-Air.localdomain>
2018-10-29 14:28:52 -0700

    errors included for debugging

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,36 @@
+class GameController < ApplicationController
+    def try
+
+        if params[:name] != ""
+            cookies[:name] = params[:name]
+        end
+
+        if session[:number] == nil
+            session[:number] = rand(1..100)
+        end
+
+        if session[:number].to_i > params[:number].to_i
+            @result = "Too low"
+        elsif session[:number].to_i < params[:number].to_i
+            @result = "Too high"
+        else
+            @result = "Correct!"
+            session[:number] = nil
+        end
+
+        if session[:number] == nil
+            cookies[:counter] = 0
+        else
+            cookies[:counter] = cookies[:counter].to_i
+        end
+
+        render "try.html.erb"
+    end
+
+    def reset
+        session[:number] = nil
+        cookies[:counter] = 0
+        cookies[:name] = ""
+        render "try.html.erb"
+    end
+end
